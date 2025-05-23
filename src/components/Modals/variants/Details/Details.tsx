ccbc1b2714d57f083a83440d538e2f9e200f477b
Szymon Paluch <12szymon.paluch34@gmail.com>
2025-05-23 17:36:27 +0200

    feat: add supply APR chart

--- src/components/Modals/variants/Details/Details.tsx
+++ src/components/Modals/variants/Details/Details.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
+import * as Recharts from "recharts";
 import { ModalLayout } from "@/components/Modals/ModalLayout/ModalLayout";
 import { Button, ModalProps, Text, CurrencyIcon, Icon } from "@/components";
 import styles from "./Details.module.scss";
-import cx from "classnames";
-import { AmountInput } from "@/components/AmountInput/AmountInput";
+import Data from "../../../Overview/Chart/mock.json";
 
 export type DetailsProps = {};
 
@@ -32,7 +32,9 @@ export const Details: React.FC<Details> = ({ props, ...rest }) => {
           </div>
           <div className={styles.main}>
             <div></div>
-            <div></div>
+            <div>
+              <Chart />
+            </div>
             <div>
               <div className={styles.infoLabel}>
                 <Text size={14} theme={600} className={styles.label}>
@@ -94,5 +96,65 @@ export const Details: React.FC<Details> = ({ props, ...rest }) => {
         </div>
       </div>
     </ModalLayout>
+  );
+};
+
+export const Chart: React.FC = () => {
+  const data = Data.map((item) => ({
+    date: item.date,
+    apr: (Math.random() * 5).toFixed(2),
+  }));
+
+  return (
+    <div className={styles.chart}>
+      <div className={styles.chartContainer}>
+        <Recharts.ResponsiveContainer>
+          <Recharts.ComposedChart
+            margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
+            data={data}
+          >
+            <Recharts.Legend
+              verticalAlign="top"
+              align="left"
+              iconSize={8}
+              height={36}
+              iconType="circle"
+              formatter={(value) => (value === "apr" ? "Supply APR" : value)}
+            />
+            <Recharts.CartesianGrid
+              strokeDasharray="3 4"
+              stroke="#2E2C37"
+              vertical={false}
+            />
+            <Recharts.XAxis
+              padding={{ left: 0, right: 0 }}
+              axisLine={{ stroke: "#2E2C37" }}
+              tickLine={false}
+              tick={{ stroke: "#2E2C37", fontSize: 12 }}
+              dataKey="date"
+            />
+            <Recharts.YAxis
+              axisLine={false}
+              tickLine={false}
+              tickFormatter={(tick) => `${tick}%`}
+              tick={{ stroke: "#2E2C37", fontSize: 12 }}
+            />
+            <Recharts.Line
+              dataKey={"apr"}
+              stroke={"#2EBAC6"}
+              strokeWidth={2}
+              dot={false}
+              activeDot={{
+                stroke: "#2EBAC6",
+                fill: "#0F0E11",
+                strokeWidth: 2,
+                width: 12,
+                height: 12,
+              }}
+            />
+          </Recharts.ComposedChart>
+        </Recharts.ResponsiveContainer>
+      </div>
+    </div>
   );
 };
