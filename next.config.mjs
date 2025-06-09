993f9aa3a9a3a73ba35d29e582d2b2bc71aa7501
afolabidamilare08 <afolabidamilare08@gmail.com>
2025-06-09 18:13:57 +0100

    Remove commented-out code and clean up next.config.mjs for clarity.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/.well-known/appspecific/com.chrome.devtools.json",
-//         destination: "/api/devtools.json",
-//       },
-//     ];
-//   },
-// };
 
 // export default nextConfig;
 /** @type {import('next').NextConfig} */
